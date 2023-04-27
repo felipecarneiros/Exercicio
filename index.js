4f8e6169a7b7c4ed4a64ebda8df20c08f5e7963f
felipecarneiros <fcs_sl@hotmail.com>
2023-04-27 08:55:14 -0300

    Exercicio

---
+++ index.js
@@ -0,0 +1,92 @@
+// console.log("hello")
+// // comentario
+// //prompt("Digite seu nome?")
+
+// const idade = prompt("Digite sua idade?")
+// console.log(idade)
+
+// const nome = prompt("Digite seu nome")
+// console.log(nome)
+
+// const endereco = prompt("Digite seu endereço:")
+// console.log(endereco)
+
+// const nome = "Felipe"
+// const idade = 33
+
+// console.log("Olá!", "Meu nome é ", nome, "e eu tenho", idade, "anos")
+
+// let nome = prompt("Digite seu nome")
+// let sobrenome = prompt("Digite seu sobrenome")
+// let idade = prompt("Digite sua idade")
+// let status = prompt("Digite se você é ou não é estudante")
+
+// console.log ("Olá!", "Meu nome é ", nome, sobrenome, "e eu tenho", idade, status)
+
+// const got = "Game of Thrones"
+// const temporadas = 8
+
+// console.log(typeof got)
+// console.log(typeof temporadas)
+
+// let a = 10
+// let b = 10
+
+// console.log(b)
+
+// b = 5
+// console.log(a, b)
+
+// Será executado 2 números 10 e um número 5
+
+// let a = 10
+// let b = 20
+// c = a
+// b = c
+// a = b
+
+// console.log(a, b, c)
+
+// Será executado 3 numeros 10
+
+// let p = prompt("Quantas horas você trabalha por dia?")
+// let t = prompt("Quanto você recebe por dia?")
+// alert(`Voce recebe ${t/p} por hora`)
+// Variáveis "hora" e "valor"
+
+// let nome 
+// let idade
+
+// console.log(typeof nome)
+// console.log(typeof idade)
+
+// nome = prompt("Digite seu nome")
+// idade = Number (prompt("Digite sua idade"))
+
+// console.log(typeof nome)
+// console.log(typeof idade)
+// console.log("Qual o seu nome?", nome, "Qual a sua idade?", idade) 
+
+// const pergunta = prompt("Você comeu hoje? (sim/nao)"
+
+// console.log("primeira pergunta: ", pergunta);
+
+let a = Number(prompt("digite um número"))
+let b = Number(prompt("digite outro número"))
+
+let c = b
+b = a
+a = c 
+
+console.log("Valor do primeiro número",a)
+console.log("Valor do segundo número",b)
+
+
+
+
+
+
+
+
+
+
